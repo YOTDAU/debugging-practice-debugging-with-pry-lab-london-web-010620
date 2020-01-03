e99de13d9d05397a1ab886447c97b19a78decebf
Simon Tucker <sjtucker28@gmail.com>
2020-01-03 11:15:57 +0000

    Automatically backed up by Learn

--- fix_using_tests/classical_gas.rb
+++ fix_using_tests/classical_gas.rb
@@ -16,14 +16,3 @@ end
 petrol_petes = GasStation.new("Petrol Pete's", 50)
 seashore_shell = GasStation.new("The Seashore Shell", 40)
 dinobones = GasStation.new("Dino Bones Gas and Grill", 60)
-
-def initialize(brand, unleaded_price)
-    @brand = brand
-    @unleaded_price = unleaded_price
-  end
-
-end
-
-petrol_petes = GasStation.new("Petrol Pete's", 50)
-seashore_shell = GasStation.new("The Seashore Shell", 40)
-dinobones = GasStation.new("Dino Bones Gas and Grill", 60)
